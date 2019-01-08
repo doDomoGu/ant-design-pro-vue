1f867a4e4644535ac6cf4fccc040ad4ef17d2234
doDomoGu <kaka_71936410@qq.com>
2019-01-08 18:02:51 +0800

    修改 router 结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import { constantRouterMap } from '@/config/router.config'
-
+import { constantRouterMap } from './routerMap'
 
 Vue.use(Router)
 
