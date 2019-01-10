0f2b2f7cde0fa3e8b8d561d4208a3eb03c2a7bd9
doDomoGu <kaka_71936410@qq.com>
2019-01-10 13:55:08 +0800

    使用less加载样式，支持主题配置

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+//vue.config.js  build配置
+module.exports = {
+  //publicPath: '/dist/',  //url根目录
+    css: {
+        loaderOptions: {
+            less: {
+                modifyVars: {
+                    'primary-color': '#1DA57A',
+                    'link-color': '#1DA57A',
+                    'border-radius-base': '2px',
+                },
+                javascriptEnabled: true
+            }
+        }
+    }
+}
